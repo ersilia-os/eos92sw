1694e4ac982f1141daf0135a46d6df63bdeca193
HellenNamulinda <hellennamulinda@gmail.com>
2023-06-20 10:16:09 +0300

    Refactored predict file, remove modelfile from arguments

--- model/framework/etoxpred_predict.py
+++ model/framework/etoxpred_predict.py
@@ -1,5 +1,7 @@
 import argparse
-
+import os
+import sys
+import csv
 import pandas as pd
 
 from rdkit import Chem
@@ -13,11 +15,17 @@ from joblib import load
 
 rdBase.DisableLog("rdApp.error")
 
+# parse arguments
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+CHECKPOINTS_BASEDIR = os.path.abspath(os.path.join(__file__,"../../checkpoints"))
+CHECKPOINT_FILE = "etoxpred_best_model.joblib" 
+modelfile = os.path.join(CHECKPOINTS_BASEDIR, CHECKPOINT_FILE) 
 
 def myargs():
     parser = argparse.ArgumentParser()
     parser.add_argument("--datafile", required=True, help="training data filename")
-    parser.add_argument("--modelfile", required=True, help="path to the model to load")
     parser.add_argument(
         "--outputfile",
         required=False,
@@ -28,17 +36,8 @@ def myargs():
     return args
 
 
-def load_data(filename):
-    """
-    Loading data from .smi file. And generating Morgan's fingerprints and labels
-    for the smiles data.
-    Input: filename -> path to the .smi file in the format of SmilesString\tCompoundName\tLabel
-    Output: two arrays X -> fingerprints, y -> labels
-    """
-    df = pd.read_csv(filename, sep="\t", names=["smiles", "name"])
-    smiles_list = df["smiles"].tolist()
-    mols = [Chem.MolFromSmiles(x) for x in smiles_list]
-    names = df["name"].tolist()
+def load_data(smiles_list):            
+    mols = [Chem.MolFromSmiles(x) for x in smiles_list] 
     X = []
     cnt = 0
     for mol in mols:
@@ -52,29 +51,53 @@ def load_data(filename):
         X.append(tmpX)
         cnt += 1
     X = np.array(X)
-    return X, smiles_list, names
+    return X, smiles_list
 
 
-def predict(opt):
-    df = pd.DataFrame(columns=["name", "smiles", "Tox-score", "SAscore"])
+def predict(smiles_list):
+    df = pd.DataFrame(columns=["smiles", "Tox-score", "SAscore"])
     # laod the data
-    X, smiles_list, names = load_data(opt.datafile)
+    X, smiles_list = load_data(smiles_list)
     # load the saved model and make predictions
     print("...loading models")
-    clf = load(opt.modelfile)
+    clf = load(modelfile)
     reg = SAscore()
     print("...starts prediction")
     for i in range(X.shape[0]):
         tox_score = clf.predict_proba(X[i, :].reshape((1, 1024)))[:, 1]
-        sa_score = reg(smiles_list[i])
-        df.at[i, "name"] = names[i]
+        sa_score = reg(smiles_list[i]) 
         df.at[i, "smiles"] = smiles_list[i]
         df.at[i, "Tox-score"] = tox_score[0]
         df.at[i, "SAscore"] = sa_score
     print("...prediction done!")
-    df.to_csv(opt.outputfile, index=False)
+    # df.to_csv(output_file, index=False)
+    output = df[['smiles','Tox-score','SAscore']].values.tolist()  
+    print('returning output')
+    print(output)
+    return output
+ 
+# read SMILES from .csv file, assuming one column with header
+with open(input_file, "r") as f:
+    reader = csv.reader(f)
+    next(reader)  # skip header
+    smiles_list = [r[0] for r in reader]
+
+# run model
+outputs = predict(smiles_list)
+
+#check input and output have the same lenght
+input_len = len(smiles_list)
+output_len = len(outputs)
+assert input_len == output_len
+
+# write output in a .csv file
+with open(output_file, "w") as f:
+    print('saving output')
+    writer = csv.writer(f)
+    writer.writerow(['smiles', 'Tox-score', 'SAscore'])  # header
+    for r in outputs:
+        print(r)
+        writer.writerow(r)
+    print('saving output done')
 
 
-if __name__ == "__main__":
-    opt = myargs()
-    predict(opt)
